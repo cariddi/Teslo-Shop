5060d718d07f4975f9ba40b3b8f81660d8b6a894
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2022-11-01 13:28:57 +0100

    serve files in a controlled way

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,11 +1,15 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
 import { fileFilter, fileNamer } from './helpers';
@@ -14,13 +18,23 @@ import { fileFilter, fileNamer } from './helpers';
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
+  @Get('product/:imageName')
+  findProductImage(
+    // when using the @Res() decorator we're skipping a few steps into Nest lifecycle such as global interceptors and restrictions
+    @Res() res: Response, // this means I manually will return my response
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
+
   @Post('product')
   @UseInterceptors(
     // 'file' this is the name of the KEY sent
     FileInterceptor('file', {
       fileFilter: fileFilter,
       storage: diskStorage({
-        destination: './static/uploads',
+        destination: './static/products',
         filename: fileNamer,
       }),
     }),
@@ -31,9 +45,10 @@ export class FilesController {
     }
 
     console.log({ file });
+    const secureUrl = `${file.filename}`;
 
     return {
-      fileName: file.originalname,
+      secureUrl,
     };
   }
 }
