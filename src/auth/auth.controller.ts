e4d643748d59f252b372ea6438cec8e06d9b89bf
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2022-11-19 23:38:30 +0100

    Adding custom guard to get metadata roles

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+  SetMetadata,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { GetUser, GetRawHeaders } from './decorators';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { UserRolesGuard } from './guards/user-roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -35,6 +44,16 @@ export class AuthController {
       user,
       userEmail,
       rawHeaders,
+    };
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards(AuthGuard(), UserRolesGuard)
+  privateRoute2(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
     };
   }
 }
---
+++ src/auth/guards/user-roles.guard.ts
@@ -0,0 +1,21 @@
+import { Reflector } from '@nestjs/core';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class UserRolesGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const validRoles: string[] = this.reflector.get(
+      'roles',
+      context.getHandler(),
+    );
+
+    console.log('validRoles: ', validRoles);
+
+    return true;
+  }
+}
