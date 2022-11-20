f6d9228266adec80f7eb7e6b4b579cce443b271e
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2022-11-20 20:05:58 +0100

    Using Auth in other modules

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,11 +3,12 @@ import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product, ProductImage } from './entities';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [TypeOrmModule.forFeature([Product, ProductImage]), AuthModule],
   exports: [ProductsService, TypeOrmModule], // need to export typeor as well to allow use to the Product & ProductImage repositories
 })
 export class ProductsModule {}
