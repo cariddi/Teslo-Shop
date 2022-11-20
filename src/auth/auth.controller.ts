7eb6536c881533c0227a88a62ce703b8ac1a64aa
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2022-11-20 21:45:50 +0100

    Adding Tags, ApiProperty and ApiResponse

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { GetUser, GetRawHeaders, RoleProtected, Auth } from './decorators';
 import { CreateUserDto, LoginUserDto } from './dto';
@@ -7,6 +8,7 @@ import { User } from './entities/user.entity';
 import { UserRolesGuard } from './guards/user-roles.guard';
 import { ValidRoles } from './interfaces/valid-roles.interface';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -25,36 +27,6 @@ export class AuthController {
   @Auth()
   CheckAuthStatus(@GetUser() user: User) {
     return this.authService.checkAuthStatus(user);
-  }
-
-  @Get('private')
-  @UseGuards(AuthGuard())
-  testingPrivateRoute(
-    // @Req() request: Express.Request,
-    @GetUser() user: User,
-    @GetUser('email') userEmail: string,
-    @GetRawHeaders() rawHeaders: string[],
-  ) {
-    // console.log(request);
-
-    return {
-      ok: true,
-      message: 'Hello World Private',
-      user,
-      userEmail,
-      rawHeaders,
-    };
-  }
-
-  @Get('private2')
-  // @SetMetadata('roles', ['admin', 'super-user']) // ---> replaced by RoleProtected()
-  @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
-  @UseGuards(AuthGuard(), UserRolesGuard)
-  privateRoute2(@GetUser() user: User) {
-    return {
-      ok: true,
-      user,
-    };
   }
 
   @Get('private3')
--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { Auth } from 'src/auth/decorators';
-import { ValidRoles } from 'src/auth/interfaces';
+import { ApiTags } from '@nestjs/swagger';
 import { SeedService } from './seed.service';
 
+@ApiTags('Seed')
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
-  // @Auth(ValidRoles.admin, ValidRoles.superUser)
   excecuteSeed() {
     return this.seedService.runSeed();
   }
