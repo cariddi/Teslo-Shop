33f9c9a80126cc8568061360a2b762e3a72ee456
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2022-11-24 15:09:49 +0100

    Identify connected clients + get amount of connected clients

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -12,9 +12,11 @@ export class MessagesWsGateway
 {
   constructor(private readonly messagesWsService: MessagesWsService) {}
   handleConnection(client: Socket) {
-    console.log('Client connected: ', client.id);
+    this.messagesWsService.registerClient(client);
+    console.log({ connected: this.messagesWsService.getConnectedClients() });
   }
   handleDisconnect(client: Socket) {
-    console.log('Client disconnected: ', client.id);
+    this.messagesWsService.removeClient(client.id);
+    console.log({ connected: this.messagesWsService.getConnectedClients() });
   }
 }
