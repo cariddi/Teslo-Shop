56b093df06b733826c7bda2557ee35a321bd2765
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2022-11-24 16:23:30 +0100

    Get connected clients and communicate that to client

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,21 +2,34 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
+import { ServerEvents } from './messages-ws.interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wss: Server;
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
   handleConnection(client: Socket) {
     this.messagesWsService.registerClient(client);
-    console.log({ connected: this.messagesWsService.getConnectedClients() });
+
+    this.wss.emit(
+      ServerEvents.ClientsUpdated,
+      this.messagesWsService.getConnectedClients(),
+    );
   }
+
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
-    console.log({ connected: this.messagesWsService.getConnectedClients() });
+
+    this.wss.emit(
+      ServerEvents.ClientsUpdated,
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 }
---
+++ src/messages-ws/messages-ws.interfaces.ts
@@ -0,0 +1,3 @@
+export enum ServerEvents {
+  ClientsUpdated = 'clients-updated',
+}
